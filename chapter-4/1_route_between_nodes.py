b817695a10eab70e130bb7d9fbd4cde610c2a40e
guo shanshan <shanminlin@gmail.com>
2019-08-14 00:04:50 +0800

    Add 4.1 route between nodes solution

--- chapter-4/1_route_between_nodes.py
+++ chapter-4/1_route_between_nodes.py
@@ -6,28 +6,81 @@ Given a directed graph, design an algorithm to find out whether there is a route
 """
 
 
-def fibonacci(n):
-    if n < 1:
-        print('Incorrect input.')
-
-    fib = {1: 0, 2: 1}  # used a dict to memorize the calculated Fibonacci Number
-    for i in range(3, n + 1):
-        fib[i] = fib[i - 1] + fib[i - 2]
-    return fib[n]
-
-print(fibonacci(2))
-print(fibonacci(8))
-
-def fibonacci2(n):
-    if n < 1:
-        print('Incorrect input.')
-    elif n == 1:
-        return 0
-    elif n == 2:
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+def find_route(node1, node2):
+    found_path = None
+    queue = Queue()
+    node = node1
+    node.shortest_path = [node]
+    all_visited_nodes = [node]
+    while node:
+        for adjacent in node.adjacency_list:
+            if not adjacent.shortest_path:
+                adjacent.shortest_path = node.shortest_path + [adjacent]
+                if adjacent == node2:
+                    found_path = node.shortest_path + [adjacent]
+                    break
+                queue.add(adjacent)
+                all_visited_nodes.append(adjacent)
+        node = queue.remove()
+    for visited in all_visited_nodes:
+        visited.shortest_path = None
+    return found_path
 
 
-print(fibonacci2(2))
-print(fibonacci2(8))
+class Node():
+    def __init__(self, data, adjacency_list=None):
+        self.data = data
+        self.adjacency_list = adjacency_list or []
+        self.shortest_path = None
+
+    def add_edge_to(self, node):
+        self.adjacency_list += [node]
+
+    def __str__(self):
+        return self.data
+
+
+class Queue():
+    def __init__(self):
+        self.array = []
+
+    def add(self, item):
+        self.array.append(item)
+
+    def remove(self):
+        if not len(self.array):
+            return None
+        item = self.array[0]
+        del self.array[0]
+        return item
+
+
+import unittest
+
+
+def str_for(path):
+    if not path: return str(path)
+    return ''.join([str(n) for n in path])
+
+
+class Test(unittest.TestCase):
+    def test_find_route(self):
+        node_j = Node('J')
+        node_i = Node('I')
+        node_h = Node('H')
+        node_d = Node('D')
+        node_f = Node('F', [node_i])
+        node_b = Node('B', [node_j])
+        node_g = Node('G', [node_d, node_h])
+        node_c = Node('C', [node_g])
+        node_a = Node('A', [node_b, node_c, node_d])
+        node_e = Node('E', [node_f, node_a])
+        node_d.add_edge_to(node_a)
+        self.assertEqual(str_for(find_route(node_a, node_i)), 'None')
+        self.assertEqual(str_for(find_route(node_a, node_j)), 'ABJ')
+        node_h.add_edge_to(node_i)
+        self.assertEqual(str_for(find_route(node_a, node_i)), 'ACGHI')
+
+
+if __name__ == "__main__":
+    unittest.main()
