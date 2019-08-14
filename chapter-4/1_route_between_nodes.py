e051a70feffacb3629ce5f6832a09b89bb14ae98
guo shanshan <shanminlin@gmail.com>
2019-08-14 16:33:33 +0800

    Update 4.1 route between nodes solution

--- chapter-4/1_route_between_nodes.py
+++ chapter-4/1_route_between_nodes.py
@@ -3,83 +3,117 @@ Chapter 4 - Problem 4.1 - Route Between Nodes
 Problem:
 Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
 
+Solution:
+1. Clarify the question:
+Repeat the question: We are a given directed graph for example:
+
+A----->B----->C
+ \     |
+  \    |
+   \   |
+    \  v
+     ->D----->E
+
+And for given two nodes for example A and E, we need to check whether there is a node between them.
+
+Clarify assumptions: - Is there a loop?
+                       (Assume yes.)
+
+2. Inputs and outputs:
+So we're taking in two nodes in a directed graph and returning True or False for whether there is a route between them.
+
+3. Test and edge cases:
+edge: We can also take in an empty node or None object.
+test: We can also take in regular inputs like this: node B and E in the above example, and we need to return True.
+
+4. Brainstorm solution:
+We can use either Breadth First Search (BFS) or Depth First Search (DFS) to find path between two nodes. So we can start
+with one node, perform a BFS or DFS. If we see the other node in our search, return true. Otherwise return false.
+
+5. Runtime analysis:
+Time complexity: O(V+E) in worst-case as we need to traverse all vertices and edges. V denotes vertice and E denotes edges.
+Space complexity: O(V+E) in worst-case
+
+6. Code
 """
-
-
-def find_route(node1, node2):
-    found_path = None
-    queue = Queue()
-    node = node1
-    node.shortest_path = [node]
-    all_visited_nodes = [node]
-    while node:
-        for adjacent in node.adjacency_list:
-            if not adjacent.shortest_path:
-                adjacent.shortest_path = node.shortest_path + [adjacent]
-                if adjacent == node2:
-                    found_path = node.shortest_path + [adjacent]
-                    break
-                queue.add(adjacent)
-                all_visited_nodes.append(adjacent)
-        node = queue.remove()
-    for visited in all_visited_nodes:
-        visited.shortest_path = None
-    return found_path
-
-
-class Node():
-    def __init__(self, data, adjacency_list=None):
-        self.data = data
-        self.adjacency_list = adjacency_list or []
-        self.shortest_path = None
-
-    def add_edge_to(self, node):
-        self.adjacency_list += [node]
-
-    def __str__(self):
-        return self.data
-
-
-class Queue():
-    def __init__(self):
-        self.array = []
-
-    def add(self, item):
-        self.array.append(item)
-
-    def remove(self):
-        if not len(self.array):
-            return None
-        item = self.array[0]
-        del self.array[0]
-        return item
-
-
 import unittest
 
 
-def str_for(path):
-    if not path: return str(path)
-    return ''.join([str(n) for n in path])
+class GraphNode:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+    def add_child(self, new_node):
+        self.children.append(new_node)
+
+    def remove_child(self, del_node):
+        if del_node in self.children:
+            self.children.remove(del_node)
 
 
+class Graph:
+    def __init__(self, node_list):
+        self.nodes = node_list
+
+    def add_edge(self, node1, node2):
+        if node1 in self.nodes and node2 in self.nodes:
+            node1.add_child(node2)
+
+    def remove_edge(self, node1, node2):
+        if node1 in self.nodes and node2 in self.nodes:
+            node1.remove_child(node2)
+
+
+def check_route(start_node, end_node):
+    """Checks whether there is a route between two nodes in a directed graph."""
+    if not isinstance(start_node, GraphNode) or not isinstance(end_node, GraphNode):
+        return None
+
+    visited = []
+
+    # Create a stack for DFS
+    stack = [start_node]
+
+    while stack:
+        # pop a node from stack
+        current_node = stack.pop()
+        visited.append(current_node)
+
+        # if this node is the end_node, return True
+        if current_node.value == end_node.value:
+            return True
+
+        # else, continue with DFS
+        for child in current_node.children:
+            if child not in visited:
+                stack.append(child)
+
+    # if DFS is completed without encountering end_node, return False
+    return False
+
+
+# 7. Test and debug
 class Test(unittest.TestCase):
-    def test_find_route(self):
-        node_j = Node('J')
-        node_i = Node('I')
-        node_h = Node('H')
-        node_d = Node('D')
-        node_f = Node('F', [node_i])
-        node_b = Node('B', [node_j])
-        node_g = Node('G', [node_d, node_h])
-        node_c = Node('C', [node_g])
-        node_a = Node('A', [node_b, node_c, node_d])
-        node_e = Node('E', [node_f, node_a])
-        node_d.add_edge_to(node_a)
-        self.assertEqual(str_for(find_route(node_a, node_i)), 'None')
-        self.assertEqual(str_for(find_route(node_a, node_j)), 'ABJ')
-        node_h.add_edge_to(node_i)
-        self.assertEqual(str_for(find_route(node_a, node_i)), 'ACGHI')
+    def test_check_route(self):
+        # create a graph
+        nodeA = GraphNode('A')
+        nodeB = GraphNode('B')
+        nodeC = GraphNode('C')
+        nodeD = GraphNode('D')
+        nodeE = GraphNode('E')
+
+        graph1 = Graph([nodeA, nodeB, nodeC, nodeD, nodeE])
+        graph1.add_edge(nodeA,nodeB)
+        graph1.add_edge(nodeA,nodeD)
+        graph1.add_edge(nodeB,nodeC)
+        graph1.add_edge(nodeB,nodeD)
+        graph1.add_edge(nodeD,nodeE)
+
+        self.assertTrue(check_route(nodeA, nodeE))
+        self.assertFalse(check_route(nodeC, nodeE))
+        self.assertTrue(check_route(nodeA, nodeA))
+        self.assertIsNone(check_route(nodeA, None))
 
 
 if __name__ == "__main__":
